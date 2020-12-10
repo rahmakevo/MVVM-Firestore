9a66503a855af4b48146d1b0f2e221684bd054b9
Kevin Siundu <kevin.siundu@equitybank.co.ke>
2020-12-10 14:46:37 +0300

    embed view controllers on main

---
+++ InstagramFirestoreClone/Controller/ImageSelectorViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ImageSelectorViewController.swift
+//  InstagramFirestoreClone
+//
+//  Created by Kevin Siundu on 10/12/2020.
+//  Copyright © 2020 Kevin Siundu. All rights reserved.
+//
+
+import Foundation
