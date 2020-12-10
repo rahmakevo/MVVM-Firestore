9d49a5773d403d5bb85e078e2f72d7bd6c2fd95f
Kevin Siundu <kevin.siundu@equitybank.co.ke>
2020-12-10 14:35:56 +0300

    setting up rootViewController programmatically

---
+++ InstagramFirestoreClone/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreClone
+//
+//  Created by Kevin Siundu on 10/12/2020.
+//  Copyright © 2020 Kevin Siundu. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
